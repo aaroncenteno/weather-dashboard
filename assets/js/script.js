3b2d0072481dc484262d83f2e0d3cfb3c0d0b56a
ASC8 <asamcent@gmail.com>
2020-09-11 15:45:25 -0700

    prevent default added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var apiKey = "130e45b8035df0b2f3d2389f4fb66852"
 
 $(document).ready(function(){
+    $("#search-btn").on("click", function (){
+        event.preventDefault();
+        var cityInput = $("#city-input").val();
+        $("#city-input").val("")
+    })
     
-
+    
 })
