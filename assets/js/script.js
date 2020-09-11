51c7d0293d3584f0f47f570c58e38ba5bd185c16
ASC8 <asamcent@gmail.com>
2020-09-11 16:17:24 -0700

    fetch created for city weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,18 @@ $(document).ready(function(){
         event.preventDefault();
         var cityInput = $("#city-input").val();
         $("#city-input").val("")
+        searchWeatherInfo(cityInput);
     })
     
-    
+    function searchWeatherInfo(cityInput) {
+        $.ajax({
+            method: "GET",
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&APPID=" + apiKey + "&units=imperial",
+            dataType: "json",
+            success: function(data) {
+                console.log(data); 
+            }
+        })
+        
+    }
 })
