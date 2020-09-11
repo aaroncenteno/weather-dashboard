6ac869a15e5d47fc9eef270e028c5b980febfd1c
ASC8 <asamcent@gmail.com>
2020-09-11 16:50:07 -0700

    Archive Searches Cities Function Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,15 @@ $(document).ready(function(){
         $("#city-input").val("")
         searchWeatherInfo(cityInput);
     })
+
+    $(".archive").on("click", "li", function() {
+        searchWeatherInfo($(this).text());
+    })
+
+    function listItems(citiesHistory) {
+        var li = $("<li>").addClass("list-group-item list-group-item-action").text(citiesHistory);
+        $(".archive").append(li);
+    }
     
     function searchWeatherInfo(cityInput) {
         $.ajax({
@@ -14,9 +23,21 @@ $(document).ready(function(){
             url: "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&APPID=" + apiKey + "&units=imperial",
             dataType: "json",
             success: function(data) {
-                console.log(data); 
+                console.log(data);
+                if (archive.indexOf(cityInput) === -1) {
+                    listItems(cityInput);
+                }
             }
         })
         
+    }
+
+    var archive = JSON.parse(window.localStorage.getItem("archive")) || [];
+
+    if (archive.length < 0) {
+        searchWeatherInfo(archive[archive.length - 1]);
+    }
+    for (var i = 0; i < archive.length; i++) {
+        listItems(archive[i]);
     }
 })
